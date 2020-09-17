ed492ec7231b9f5f0ca24543d2d3d42f2f944082
Toyofumi Takeda <t_takeda@w-endless.co.jp>
2020-09-17 16:31:08 +0900

    add checkstyle

---
+++ src/main/java/com/github/mdstoy/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.github.mdstoy.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping()
+    public String hello() {
+            int x = 0;
+        return String.format("hello, world. [%s]", LocalDateTime.now());
+    }
+}
